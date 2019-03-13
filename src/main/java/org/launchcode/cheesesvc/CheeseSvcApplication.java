a822c6df72b166e43b8274e6403e31f6543a9cc1
Brian Hirtzer <bhirtzer@gmail.com>
2019-03-12 21:00:49 -0500

    Initial Commit

---
+++ src/main/java/org/launchcode/cheesesvc/CheeseSvcApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.cheesesvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeseSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheeseSvcApplication.class, args);
+	}
+
+}
